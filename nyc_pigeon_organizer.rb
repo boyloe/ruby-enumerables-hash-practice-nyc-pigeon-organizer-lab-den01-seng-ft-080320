82e6a01d0245a2c209f5fefc5863d52ac0989484
Bryan Oyloe <boyloe@gmail.com>
2020-06-23 18:20:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = {}  
+  name_array = []
+  data[:color][:purple].collect do |element|
+      name_array << element
+  end
+  name_array
+  binding.pry
+  
 end
