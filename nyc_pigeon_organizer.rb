e996302fe14b34d3924fc33521500c2e0e3963b6
Bryan Oyloe <boyloe@gmail.com>
2020-06-23 18:23:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ require "pry"
 def nyc_pigeon_organizer(data)
  
  data.each_with_object({}) do |key, value, new_array|
-   
+   data.each do |inner_key , names|
+     binding.pry
   binding.pry
 end  
   
