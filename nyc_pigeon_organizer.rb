dca375dad580c88fe3e077110549e38b782d58bb
Bryan Oyloe <boyloe@gmail.com>
2020-06-23 18:53:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
       names.each do |name|
-        new_array[name.to_sym]
+        new_array[:name]
       end  
     end  
     # binding.pry
